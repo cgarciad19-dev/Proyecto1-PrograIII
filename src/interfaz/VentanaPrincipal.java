19e1defda91c013af913a1da56eb7471a181946c
Cristian Garcia <cgarciad19@miumg.edu.gt>
2026-03-17 23:32:45 -0600

    Update and rename Proyecto1/src/main/java/interfaz/VentanaPrincipal.java to src/interfaz/VentanaPrincipal.java

--- src/interfaz/VentanaPrincipal.java
+++ src/interfaz/VentanaPrincipal.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package interfaz;
 
-/**
- *
- * @author Morales
- */
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
